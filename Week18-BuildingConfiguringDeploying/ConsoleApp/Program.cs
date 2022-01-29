2e80b5056913e4473583de44ee3b77e6fbaca66d
Vladimirs <vladimirribakov123@mail.ru>
2022-01-29 14:20:33 +0000

    Finished Lab Week 18

---
+++ Week18-BuildingConfiguringDeploying/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Console = Colorful.Console;
+using System.Configuration;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string greeting = Properties.Settings.Default.GreetingMessage;
+            Console.WriteLine(greeting);            string tiredMessage = ConfigurationManager.AppSettings["tiredMessage"];
+            Console.WriteLine(tiredMessage);            Console.WriteLine("Program has finished. Press enter to quit.", Color.Aquamarine);            Console.ReadKey();
+
+
+        }
+    }
+}
